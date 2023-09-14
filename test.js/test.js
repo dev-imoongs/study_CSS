b7eedcd4b8ffc425c215d5765076c4a68baea17b
dev-imoongs <dev.imoong@gmail.com>
2023-09-14 23:10:34 +0900

    study 자료 업로드

---
+++ test.js/test.js
@@ -0,0 +1,11 @@
+// function solution(numbers, hand) {
+//   let all = [[1,4,7],[2,5,8,0],[3,6,9]]
+//   let L = all[0];
+//   let C = all[1];
+//   let R = all[2];
+  
+//   let answer = '';
+//   return answer;
+// }
+
+let all = [[1,4,7],[2,5,8,0],[3,6,9]]
